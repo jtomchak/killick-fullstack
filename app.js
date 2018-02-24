cf422958d74125aa086fc5eb0e579db3024aa4ab
Jesse Tomchak <jtomchak@gmail.com>
2018-02-23 17:56:43 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,95 @@
+var http = require("http"),
+  path = require("path"),
+  methods = require("methods"),
+  express = require("express"),
+  bodyParser = require("body-parser"),
+  session = require("express-session"),
+  cors = require("cors"),
+  passport = require("passport"),
+  errorhandler = require("errorhandler"),
+  mongoose = require("mongoose");
+
+var isProduction = process.env.NODE_ENV === "production";
+
+// Create global app object
+var app = express();
+
+app.use(cors());
+
+// Normal express config defaults
+app.use(require("morgan")("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(require("method-override")());
+app.use(express.static(__dirname + "/public"));
+
+app.use(
+  session({
+    secret: "killick_cookietown",
+    cookie: { maxAge: 60000 },
+    resave: false,
+    saveUninitialized: false
+  })
+);
+
+if (!isProduction) {
+  app.use(errorhandler());
+}
+
+if (isProduction) {
+  mongoose.connect(process.env.MONGODB_URI);
+} else {
+  mongoose.connect("mongodb://localhost/killick");
+  mongoose.set("debug", true);
+}
+
+require("./models/User");
+require("./models/Article");
+require("./models/Comment");
+require("./config/passport");
+
+app.use(require("./routes"));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+/// error handlers
+
+// development error handler
+// will print stacktrace
+if (!isProduction) {
+  app.use(function(err, req, res, next) {
+    console.log(err.stack);
+
+    res.status(err.status || 500);
+
+    res.json({
+      errors: {
+        message: err.message,
+        error: err
+      }
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    errors: {
+      message: err.message,
+      error: {}
+    }
+  });
+});
+
+// finally, let's start our server...
+var server = app.listen(process.env.PORT || 3000, function() {
+  console.log("Listening on port " + server.address().port);
+});
